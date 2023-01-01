38fe2651ffe12cdd6f9c7a6c0140da42843e29b4
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-01-01 16:59:45 +0100

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -17,7 +17,7 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    function _createZombie(string memory _name, uint _dna) internal {
         zombies.push(Zombie(_name, _dna));
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
