22b99b68a2bfab1138b77c4692ac8b332e37dbfa
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-01-02 15:34:17 +0100

    Update zombieFactory.sol

--- zombieFactory.sol
+++ zombieFactory.sol
@@ -8,13 +8,14 @@ contract ZombieFactory is Ownable {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
+
 
     struct Zombie {
         string name;
         uint dna;
         uint32 level;
         uint32 readyTime;
-        
     }
 
     Zombie[] public zombies;
@@ -23,7 +24,9 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // 2. Update the following line:
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + coolsownTime))) - 1;
+        
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
