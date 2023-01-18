ef45d7dc685532a4a48d7634c32bf35e12666166
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-01-18 11:27:12 +0100

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -6,7 +6,7 @@ contract ZombieFactory {
      uint dnaDigits = 16;
 
    
-    uint dnaModulus = 101 ** dnaDigits;
+    uint dnaModulus = 10 ** dnaDigits;
 
     struct Zombie {
         string name;
