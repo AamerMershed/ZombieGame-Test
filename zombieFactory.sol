fe64cf0cb346fc9c1ef1e9f1ac38949892e9bec8
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-02-27 07:48:36 +0100

    Update zombieFactory.sol

--- zombieFactory.sol
+++ zombieFactory.sol
@@ -6,7 +6,7 @@ contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
-    uint dnaDigits = 16000;
+    uint dnaDigits = 160000;
     uint dnaModulus = 10 ** dnaDigits;
     uint cooldownTime = 1 days;
 
