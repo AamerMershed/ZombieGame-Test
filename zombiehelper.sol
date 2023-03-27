44f8a3368ea528c372dcfa836d24c90842e318dc
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-03-27 19:42:49 +0200

    Update zombiehelper.sol

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -24,7 +24,7 @@ contract ZombieHelper is ZombieFeeding {
     // Start here
 
     uint counter = 0;
-    for (uint i = 00; i < zombies.length; i++) {
+    for (uint i = 0; i < zombies.length; i++) {
       if (zombieToOwner[i] == _owner) {
         result[counter] = i;
         counter++;
